f55ad36b0f7768357883714d937fa77dcb5d0fe2
Aleem Awan <muhammadaleem366@gmail.com>
2022-06-23 01:09:47 +0500

    adds buildSchema for graphql and root value

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express")
+const {buildSchema} = require("graphql")
+const {graphqlHTTP} = require("express-graphql")
+const schema = buildSchema(`
+type Query{
+name:String
+price:Float
+}
+`)
+const root = {
+    name:"Ice Cream Shake",
+    price:12.23
+}
 const app = express()
+
+app.use("/graphql",graphqlHTTP({
+    schema:schema,
+    rootValue:root
+}))
 const port = 3000
-app.listen(port,()=>{
+app.listen(port,() => {
     console.log(`Listening on port ${port}`)
 })
