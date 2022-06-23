67e724ab1fe7e19aaee54a38b9a23f90008198a1
Aleem Awan <muhammadaleem366@gmail.com>
2022-06-24 00:47:32 +0500

    adds products.graphql and orders.graphql to define orders and products types structures

---
+++ orders/orders.model.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        data:"2123-32-2",
+        subtotal:5,
+        items:[
+            {
+                product:{
+                    id:"redshoe",
+                    description:"Old Red Shoe",
+                    price: 76.34
+                },
+                quantity:2
+            }
+        ]
+
+    }
+]
---
+++ products/products.model.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {
+        id:"redshoe",
+        description:"Red Shoe",
+        price:42.2
+    },{
+        id:"bluejeans",
+        description: "Blue Jeans",
+        price: 54.34
+    }
+]
